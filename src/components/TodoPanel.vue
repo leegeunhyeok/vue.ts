a500cf1f3d4454044d662d02e4389bf9f9bbef6f
leegeunhyeok <lghlove0509@naver.com>
2019-04-24 18:18:57 +0900

    ADD: Input field with style

--- src/components/TodoPanel.vue
+++ src/components/TodoPanel.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="todo-panel">
+    <div class="todo-panel__input">
+      <input v-model="text"
+        @keydown.enter="addItem"
+      >
+    </div>
     <TodoItem v-for="(item, i) in todo"
       :key="i"
       :data="item"
@@ -24,6 +29,14 @@ import TodoItem from '@/components/TodoItem.vue';
 })
 export default class TodoPanel extends Vue {
   @Prop() private msg!: string;
+  private text: string = '';
+
+  private addItem(): void {
+    if (this.text) {
+      this.$store.commit('addItem', this.text);
+      this.text = '';
+    }
+  }
 }
 </script>
 
@@ -38,5 +51,27 @@ export default class TodoPanel extends Vue {
       -ms-transform: translate(-50%, -50%);
        -o-transform: translate(-50%, -50%);
           transform: translate(-50%, -50%);
+}
+
+.todo-panel__input {
+  margin-bottom: 1rem;
+}
+
+.todo-panel__input input {
+  outline: none;
+  padding: .4rem .8rem;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0px 0px 3px rgba(0, 0, 0, .3);
+  -webkit-transition: .3s;
+     -moz-transition: .3s;
+      -ms-transition: .3s;
+       -o-transition: .3s;
+          transition: .3s;
+}
+
+.todo-panel__input input:hover,
+.todo-panel__input input:focus {
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, .5);
 }
 </style>
