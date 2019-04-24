61825e86bfadefbc830ccc0d404256612fc72989
leegeunhyeok <lghlove0509@naver.com>
2019-04-24 17:05:26 +0900

    ADD: import TodoPanel from Home.vue

---
+++ src/components/TodoPanel.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="todo-panel">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class TodoPanel extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style scoped>
+.todo-panel {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  padding: 1rem;
+  -webkit-transform: translate(-50%; -50%);
+     -moz-transform: translate(-50%; -50%);
+      -ms-transform: translate(-50%; -50%);
+       -o-transform: translate(-50%; -50%);
+          transform: translate(-50%; -50%);
+}
+</style>
