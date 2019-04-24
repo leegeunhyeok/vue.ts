c6cd76d0c5141f9f540d253b3e590b0f0cd98f92
leegeunhyeok <lghlove0509@naver.com>
2019-04-24 18:07:29 +0900

    ADD: TodoItem.vue

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="todo-item">{{ data }}</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class TodoItem extends Vue {
+  @Prop() private data!: string;
+}
+</script>
+
+<style scoped>
+.todo-panel {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  padding: 1rem;
+  -webkit-transform: translate(-50%, -50%);
+     -moz-transform: translate(-50%, -50%);
+      -ms-transform: translate(-50%, -50%);
+       -o-transform: translate(-50%, -50%);
+          transform: translate(-50%, -50%);
+}
+</style>
