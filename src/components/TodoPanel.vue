4e61ea4476e3014bef75122e39c24d05b260fbe9
leegeunhyeok <lghlove0509@naver.com>
2019-04-24 18:08:31 +0900

    FIX: CSS property values

--- src/components/TodoPanel.vue
+++ src/components/TodoPanel.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="todo-panel">
+    <TodoItem v-for="(item, i) in todo"
+      :key="i"
+      :data="item"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import { mapState } from 'vuex';
+import TodoItem from '@/components/TodoItem.vue';
 
-@Component
+@Component({
+  components: {
+    TodoItem,
+  },
+  computed: {
+    todo() {
+      return this.$store.state.todoData;
+    },
+  },
+})
 export default class TodoPanel extends Vue {
   @Prop() private msg!: string;
 }
@@ -18,10 +33,10 @@ export default class TodoPanel extends Vue {
   top: 50%;
   left: 50%;
   padding: 1rem;
-  -webkit-transform: translate(-50%; -50%);
-     -moz-transform: translate(-50%; -50%);
-      -ms-transform: translate(-50%; -50%);
-       -o-transform: translate(-50%; -50%);
-          transform: translate(-50%; -50%);
+  -webkit-transform: translate(-50%, -50%);
+     -moz-transform: translate(-50%, -50%);
+      -ms-transform: translate(-50%, -50%);
+       -o-transform: translate(-50%, -50%);
+          transform: translate(-50%, -50%);
 }
 </style>
